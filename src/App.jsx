8d6b39bfa9b3a502a56abaebcfeb0823dfc8522f
sachin <sachinpal1199@gmail.com>
2024-07-31 21:03:40 +0530

    first
    t push -u origin test --force

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+<<<<<<< HEAD
 import Home from './Component/Home/Home'
 
 import './App.css'
+=======
+import './App.css'
+import Home from './components/Home/Home'
+>>>>>>> b844f64 (first)
 
 function App() {
 
   return (
     <>
+<<<<<<< HEAD
      <Home/>
+=======
+    <Home/>
+>>>>>>> b844f64 (first)
     </>
   )
 }
